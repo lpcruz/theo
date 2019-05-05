ae2189e4c645128979c3259efd050582f0839d15
lpcruz <laurence.p.cruz@gmail.com>
2019-05-05 13:29:14 -0400

    Remove extra tab for yelp review

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ const Yelp = require('./yelp');
 
               this.yelp.getBiz(search, location).then(biz => {
                 this.yelp.getBizReviews(biz.alias).then(review => {
-                    this.slack.notify(`I know a great place to get some${search} called <${biz.url}|${biz.name}>. It has a ${biz.rating}/5 rating:\n\n*${biz.name}*\n${biz.location.display_address}\n\nHere's what someone had to say about it:\n\n\n"${review}"`); 
+                    this.slack.notify(`I know a great place to get some${search} called <${biz.url}|${biz.name}>. It has a ${biz.rating}/5 rating:\n\n*${biz.name}*\n${biz.location.display_address}\n\nHere's what someone had to say about it:\n\n"${review}"`); 
                 })
             })
           } else {
