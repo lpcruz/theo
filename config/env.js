c1422309b01101e408c25614b1e902e8027d40ca
lpcruz <laurence.p.cruz@gmail.com>
2020-04-04 17:35:19 -0400

    (fix) lol fml another fix

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@ module.exports = {
   SLACK: {
     SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID,
     SLACK_CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET,
-    SLACK_SIGNING_SECRENT: process.env.SLACK_SIGNING_SECRENT,
+    SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRENT,
     SLACK_URI: process.env.SLACK_URI,
   },
   
