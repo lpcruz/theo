36ed44b1668993c8c2ec939f60770d41a27a2375
Laurence Cruz <lcruz@squarespace.com>
2020-05-24 21:48:52 -0400

    (refactor): Shift Yelp to async / await

--- src/API/Yelp.js
+++ src/API/Yelp.js
@@ -7,29 +7,24 @@ const client = yelp.client(
   });
 
 class Yelp { 
-  getBiz(searchTerm, location) {
-    return new Promise((resolve, reject) => {
-      client.search({
-        term: searchTerm,
-        location: location,
-      }).then(response => {
-        const biz = (response.jsonBody.businesses[0]);
-        resolve(biz);
-      }).catch(err => {
-        reject(err)
-      });
-    })
+  async getBiz(term, location) {
+    try {
+      const res = await client.search({ term, location });
+      return res.jsonBody.businesses[0];
+    } catch (e) {
+      console.error('Something went wrong!', e);
+      throw e;
+    }
   }
 
-  getBizReviews(biz) {
-    return new Promise((resolve, reject) => {
-      client.reviews(biz).then(response => {
-        const review = response.jsonBody.reviews[0].text;
-        resolve(review);
-      }).catch(err => {
-        reject(err)
-      });
-    })
+  async getBizReviews(biz) {
+    try {
+      const res = await client.reviews(biz);
+      return res.jsonBody.reviews[0].text;
+    } catch (e) {
+      console.error('Something went wrong!', e);
+      throw e;
+    }
   }
 }
 
