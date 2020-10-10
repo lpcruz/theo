afffebf047cb12957ca9f70cdc884fb373359a7b
Laurence Cruz <lcruz@squarespace.com>
2020-10-09 23:17:04 -0400

    :hammer: (refactor): async/await-ify weather report

--- src/API/Weather.js
+++ src/API/Weather.js
@@ -1,15 +1,14 @@
-const request = require('request');
 const env = require('../../config/env');
 
-class Weather { 
-  getTemp(city) {
-    return new Promise((resolve) => {
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${env.WEATHER.WEATHER_API_KEY}`
-      request(url, function (err, response, body) {
-        const json = JSON.parse(body);
-        resolve(json);
-      })
-    })
+class Weather {
+  constructor(request) {
+    this.request = request;
+  }
+  async getTemp(city) {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${env.WEATHER.WEATHER_API_KEY}`;
+    const res = await this.request(url);
+    return JSON.parse(res)
+
   }
 }
 
