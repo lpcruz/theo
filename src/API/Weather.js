1784bb9ae7a41e9a07f6e34bf9bfd15af53ee1a0
lpcruz <laurence.p.cruz@gmail.com>
2020-10-10 20:25:11 -0400

    :hammer: (refactor): Adjust weather report endpoint template literal

--- src/API/Weather.js
+++ src/API/Weather.js
@@ -2,13 +2,14 @@
 class Weather {
   constructor(request, env) {
     this.request = request;
-    this.env = env;
+    this.weatherApiKey = env.WEATHER.WEATHER_API_KEY;
   }
 
   async getTemp(city) {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${this.env.WEATHER.WEATHER_API_KEY}`;
+    const endpoint = `${city}&units=imperial&appid=${this.weatherApiKey}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${endpoint}`;
     const res = await this.request(url);
-    return JSON.parse(res)
+    return JSON.parse(res);
   }
 }
 
