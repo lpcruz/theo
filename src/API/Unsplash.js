7d1e1feb51bbfc83b0a6e1c7147d88dda397f5af
Laurence Cruz <lcruz@squarespace.com>
2020-10-12 22:52:30 -0400

    :lipstick: (style): Adding space

--- src/API/Unsplash.js
+++ src/API/Unsplash.js
@@ -4,6 +4,7 @@ class Unsplash {
     this.request = request;
     this.env = env;
   }
+
   async getPhoto(query) {
     const endpoint = `https://api.unsplash.com/search/photos/?client_id=${this.env.UNSPLASH.UNSPLASH_ACCESS_KEY}&query=${query}`;
     return await this.request(endpoint);
